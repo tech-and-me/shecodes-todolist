5698475c0022474de2948c07e0c7c20256ed3329
Pary <81phary@gmail.com>
2023-02-10 16:28:44 +1100

    adding codes from step8 to step9

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./TodoItem.css";
+
 function TodoItem(props){
     const { todo } = props;
     console.log(props);
-    return (<div>{todo}</div>)
+    return (<div className="todo">{todo}</div>)
 }
 
 export default TodoItem;
