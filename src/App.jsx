5258dc85135184395468751966bd9cb002ca62ac
Pary <81phary@gmail.com>
2023-02-10 16:51:31 +1100

    adding codes for step10

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,30 @@ import TodoItem from './components/TodoItem/TodoItem';
 
 function App(){
   const [todos, setTodos] = useState([
-    "Learn React Js",
-    "Learn Git",
-    "Build really cool todo app"
+    {text:"Learn React Js",isCompleted:false},
+    {text:"Learn git",isCompleted:false},
+    {text:"Build really cool todo app",isCompleted:false}
   ])
 
   const addTodo = (text) => {
-    const newTodos = [...todos,text];
+    const newTodos = [...todos,{text}];
     setTodos(newTodos);
   };
 
-
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  }
 
   return (
     <div className="app">
       <div className="todo-list">
-      <h1 >My todo list</h1>
-      {
-        todos.map((todo,index) => {
-          let name = "Phary"
-          return (
-      
-              <TodoItem todo={todo} key={index} />
-              // {/* <p>==={name}===</p> */}
-  
-          )
-          
-        }
-         
-        )
-      }
-      <TodoForm addTodo={addTodo}/>
+      <h1>My todo list</h1>
+      {todos.map((todo,index) => (
+              <TodoItem todo={todo} key={index} index={index} completeTodo={completeTodo}/>
+      ))}
+      <TodoForm addTodo={addTodo} />
     </div>
     </div>
   )
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -3,9 +3,16 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props){
-    const { todo } = props;
+    const { todo,index,completeTodo } = props;
     console.log(props);
-    return (<div className="todo">{todo}</div>)
+    return (
+        <div className={`todo ${todo.isCompleted ? "complete" : ""}`}>{todo.text}
+        {todo.text}
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+        </div>
+        </div>
+    )
 }
 
 export default TodoItem;
